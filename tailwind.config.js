f8532fb3fad72d90b9aedb1c573baec2fad5d8eb
Scthe <marcin1113C@gmail.com>
2024-12-04 22:48:30 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}', '!./node_modules'],
+  theme: {
+    extend: {
+      colors: ({ colors }) => {
+        return {
+          accent: colors.sky,
+          focused: colors.pink[500],
+        };
+      },
+    },
+  },
+  // CAREFUL! Be specific with regexes!
+  // https://github.com/tailwindlabs/tailwindcss/issues/8845#issuecomment-1184569469
+  safelist: [
+    {
+      pattern: /(^text|outline|bg|border|shadow|w|h)-[a-z]+-\d+$/,
+      variants: ['hover', 'focus', 'active', 'group-hover', 'before'],
+    },
+  ],
+  plugins: [],
+};
